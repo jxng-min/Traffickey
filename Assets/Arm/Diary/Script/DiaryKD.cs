67a5648da04dd96860727f4766a3c8a8876e8a12
Kim Jongmin <vwmartin@naver.com>
2025-03-06 22:16:50 +0900

    fix: 프로젝트 삭제 및 .gitignore 수정

--- Assets/Arm/Diary/Script/DiaryKD.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DiaryKD : MonoBehaviour
-{
-    public string m_diary_name;
-    public string m_diary_text;
-}
--- Assets/UI/Inventory/Script/ItemPickUpKD.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ItemPickUpKD : MonoBehaviour
-{
-    public ItemKD item;
-}
